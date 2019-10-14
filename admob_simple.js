799b05b27cefc3f0224e7c813a1425d6a1ddc70e
ezydaiman01 <36045205+ezydaiman01@users.noreply.github.com>
2019-10-14 10:05:22 +0700

    Add files via upload

---
+++ admob_simple.js
@@ -0,0 +1,33 @@
+var admobid = {};
+
+// TODO: replace the following ad units with your own
+
+  admobid = { 
+    banner: 'ca-app-pub-2009859815104934/8246685759',
+    interstitial: 'ca-app-pub-2009859815104934/5620522415'
+  };
+
+function initApp() {
+  if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+
+  // this will create a banner on startup
+  AdMob.createBanner( {
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    overlap: false,
+    offsetTopBar: false,
+    bgColor: 'black'
+  } );
+
+  // this will load a full screen ad on startup
+  AdMob.prepareInterstitial({
+    adId: admobid.interstitial,
+    autoShow: true
+  });
+}
+
+if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+    document.addEventListener('deviceready', initApp, false);
+} else {
+    initApp();
+}
